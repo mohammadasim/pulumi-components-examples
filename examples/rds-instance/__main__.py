033985fbcb8212fd5cef2b53627281a085c884e8
Mohammad Ayub <asimayub81@gmail.com>
2023-01-02 17:35:52 +0000

    adde vpc resource

--- examples/rds-instance/__main__.py
+++ examples/rds-instance/__main__.py
@@ -1,11 +1,26 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
-from pulumi_aws import s3
+from pulumi_components.aws.utils import register_tags
+from pulumi_components.aws.components.vpc import Vpc, VpcSubnetArgs
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
-s3.Bucket("another-bucket")
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+# register tags
+register_tags()
+
+conf = pulumi.Config()
+vpc_conf = conf.get_object("vpc")
+env = pulumi.get_stack().split(".")[1]
+
+public_subnets = [VpcSubnetArgs(**subnet) for subnet in vpc_conf.get("public_subnets")]
+private_subnets = [VpcSubnetArgs(**subnet) for subnet in vpc_conf.get("private_subnets")]
+
+# Create VPC
+vpc = Vpc(
+    f"{env}-vpc",
+    vpc_conf.get("vpc_cidr"),
+    public_subnets,
+    private_subnets,
+    ha_nat=False,
+)
+
