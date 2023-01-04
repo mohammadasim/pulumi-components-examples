2b2a8b0d201de933a7362e54b15586608a3635fd
Mohammad Ayub <asimayub81@gmail.com>
2023-01-04 00:17:34 +0000

    added rds variables

--- examples/rds-instance/__main__.py
+++ examples/rds-instance/__main__.py
@@ -3,6 +3,7 @@
 import pulumi
 from pulumi_components.aws.utils import register_tags
 from pulumi_components.aws.components.vpc import Vpc, VpcSubnetArgs
+from pulumi_components.aws.components.rds import RDSInstance
 
 
 # register tags
@@ -23,5 +24,23 @@ vpc = Vpc(
     private_subnets,
     ha_nat=False,
 )
+# Create an RDS instance
+rds_conf = conf.get_object("rds")
+rds_instance = RDSInstance(
+    f"{env}-rds-instance",
+    rds_conf.get("allocated_storage"),
+    rds_conf.get("instance_class"),
+    rds_conf.get("engine"),
+    rds_conf.get("engine_version"),
+    rds_conf.get("family"),
+    f"{env}-rds-instance",
+    rds_conf.get("username"),
+    pulumi.Output.secret(rds_conf.get("password")),
+    vpc.vpc.id,
+    db_name=rds_conf.get("db_name"),
+    ingress_security_group_cidrs=[vpc.vpc.cidr_block],
+    subnet_ids=vpc.private_subnet_ids
+)
+
 pulumi.export("vpc_id", vpc.vpc.id)
 
